8ceb8ebe8e28ee8dc3e52a69112b70b6810aa461
praveen <praveen.vundyala@gmail.com>
2020-10-10 22:41:55 +0530

    Initial Commit

---
+++ SwiftUIList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIList
+//
+//  Created by praveen reddy on 10/10/20.
+//  Copyright © 2020 praveen reddy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
